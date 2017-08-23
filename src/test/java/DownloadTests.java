fa71123656110f869cf1158facfd152216e238ab
Cosmin Cara <kraftek@c-s.ro>
2017-08-23 20:15:43 +0300

    Proper annotation of test methods

--- src/test/java/DownloadTests.java
+++ src/test/java/DownloadTests.java
@@ -27,6 +27,7 @@ public class DownloadTests extends TestBase {
         }
     }
 
+    @Test
     public void awsSentinel2DownloadTest() {
         Path path = getOutputFolder();
         String cmd = String.format("--out %s --tiles T35TLK --startdate 2017-07-01 --enddate 2017-07-14 --store AWS --aws",
@@ -43,6 +44,7 @@ public class DownloadTests extends TestBase {
         }
     }
 
+    @Test
     public void awsLandsat8DownloadTest() {
         Path path = getOutputFolder();
         String cmd = String.format("--out %s --sensor L8 --tiles 139045 --startdate 2017-07-09 --enddate 2017-07-11 --l8col C1 --l8pt RT",
@@ -59,6 +61,7 @@ public class DownloadTests extends TestBase {
         }
     }
 
+    @Test
     public void sciHubSentinel2BDownloadTest() {
         Path path = getOutputFolder();
         String cmd = String.format("--out %s --tiles T35TLK --startdate 2017-07-12 --enddate 2017-07-14 --user %s --password %s --preops",
